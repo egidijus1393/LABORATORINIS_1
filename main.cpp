97c49016a66a535ccc50b8750c05ac89de560257
egidijus1393 <47447462+egidijus1393@users.noreply.github.com>
2019-03-25 00:35:13 +0200

    Antra skiltis
    Įdėjau medianos skaičiavimus.

--- main.cpp
+++ main.cpp
@@ -3,16 +3,16 @@
 #include <iomanip>
 
 //apsirašiau bibliotekas
-
 using namespace std;
 
 struct Studentas {
     string vardas;
     string pavarde;
     double vid = 0;
-    int egzas = 0;
+    double med = 0;
+    int egz = 0;
 };
-//apsirašiau struktūrą
+//apsirašiau struktura
 void TikrintiPavadinima( string &pav ) {
     const int n = pav.length();
 
@@ -20,80 +20,113 @@ void TikrintiPavadinima( string &pav ) {
         if ( pav[i] >= 97 && pav[i] <= 122 && pav[0] >= 65 && pav[0] <= 90 ) {
             continue;
         } else {
-            cout << endl << "Blogai ivestas studento vardas" << endl << endl;
-            cout << "Áveskite dar kartà: ";
+            cout <<  "Blogai ivestas studento vardas"  << endl;
+            cout << "Įveskite dar kartą: ";
             cin >> pav;
 
             i = 0;
         }
     }
 }
-//apsirašiu void funkcija kuri patikrina ar įmanomas toks vardas(ar pirmas elemantas mažoji ar didžioji raidė o sekantys elementai iš mažosios) užklausiame vartotojo įvesti studento vardą jį patikriname jei vardas neegzistuoja išvedame i ekraną įveskite dar kartą.
+//apsirašiu void funkcija kuri patikrina ar imanomas toks vardas(ar pirmas elemantas mažoji ar didžioji raide o sekantys elementai iš mažosios) užklausiame vartotojo ivesti studento varda ji patikriname jei vardas neegzistuoja išvedame i ekrana iveskite dar karta.
 int GautiSkaiciu() {
     string input;
     cin >> input;
 
     while ( input.find_first_not_of( "0123456789" ) != string::npos ) {
-        cout << endl << "Blogai ávestas skaièius" << endl << endl;
-        cout << "Áveskite dar karta: ";
+        cout << "Blogai ivestas skaicius" << endl;
+        cout << "Įveskite dar karta: ";
         cin >> input;
     }
 
     return stoi( input );
 }
-
-// Sukdami while ciklą tikriname ar musu stringas nėra lygus vienam iš nurodytų "find_first_not_of" argumentų, jei yra tada gražiname gerą skaičių jei ne jis gražina" blogai įvestas skaičius".
+// Sukdami while cikla tikriname ar musu stringas nera lygus vienam iš nurodytu "find_first_not_of" argumentu, jei yra tada gražiname gera skaiciu jei ne jis gražina" blogai ivestas skaicius".
 int main() {
     setlocale( LC_ALL, "Lithuanian" );
-//Nustatome lietuvių kalbą.
+//Nustatome lietuviu kalba.
     int s;
 
-    cout << "Áveskite studentu skaièius: ";
+    cout << "Iveskite studentu skaiciu: ";
     cin >> s;
-//paprašome vartotoją įvesti studentų skaičių tą skaičių panaudojame for ciklui norodyti iki kokios reikšmės sukamas foras.
-    Studentas studentai[100];
+//paprašome vartotoja ivesti studentu skaiciu ta skaiciu panaudojame for ciklui norodyti iki kokios reikšmes sukamas foras.
+    Studentas studentai[ 100 ];
 
     for ( int i = 0; i < s; i++ ) {
-        cout << "Áveskite " << i+1 << " studento vardà: ";
+        cout << "Iveskite:" << i+1 << " studento varda: ";
         cin >> studentai[i].vardas;
         TikrintiPavadinima( studentai[i].vardas );
-//Vartotoją prašome įvesti vardą jį siunčiame į void funkciją kad patikritų ar jis egzistuoja.
+//Vartotoja prašome ivesti varda ji siunciame i void funkcija kad patikritu ar jis egzistuoja.
         cout << endl;
-        cout << "Pavardæ: ";
+        cout << "Pavarde: ";
         cin >> studentai[i].pavarde;
         TikrintiPavadinima( studentai[i].pavarde );
-//Vartotoją prašome įvesti pavardę jį siunčiame į void funkciją kad patikritų ar jis egzistuoja.
-        cout << "Studento "  << i+1 << " namu darbu skaièiø: ";
+//Vartotoja prašome ivesti pavarde ji siunciame i void funkcija kad patikritu ar jis egzistuoja.
+        cout << "Studento "<<i+1<<"namu darbu skaicius: ";
         int n    = GautiSkaiciu();
         int suma = 0;
-//Vartotoją prašome įvesti namų darbų skaičių iš funkcijos"GautiSkaičių".
-        for ( int j = 0, k; j < n; j++ ) {
-            cout << "Iveskite paþymá: ";
+//Vartotoja prašome ivesti namu darbu skaiciu iš funkcijos"GautiSkaiciu".
+        for ( int j = 1, k; j <= n; j++ ) {
+            cout << "Iveskite pazymi: ";
             k = GautiSkaiciu();
-//sukame for ciklą ir prašome vartotoją įvesti pažymį iš "GautiSkaiciu()" .
+//sukame for cikla ir prašome vartotoja ivesti pažymi iš "GautiSkaiciu()" .
             while ( k < 1 || k > 10 ) {
-                cout << "Ivestas skaièius yra per maþas/didelis." << endl;
-                cout << "Bandykite dar kartà: ";
+                cout << "Ivestas skaicius yra per mazas/didelis." << endl;
+                cout << "Bandykite dar karta: ";
                 k = GautiSkaiciu();
             }
-
-            suma = suma + k;
+//tikriname ar pažimys yra tarp vieneto ir dešimties tada siunciamas kad butu patikrinamas.
+            if ( n % 2 && j == ( n / 2 + 1 ) ) {
+                studentai[ i ].med = k;
+            } else if ( !( n % 2 ) && ( j == ( n / 2 ) || j == ( n / 2 + 1 ) ) ) {
+                studentai[i].med += k;
+            }
+//Tikrinamas medianos tipas.
+            suma += k;
         }
-//tikriname ar pažimys yra tarp vieneto ir dešimties tada siunčiamas kad butų patikrinamas.
-        studentai[i].vid = suma / n;
-//vidurkį apskaičiuojame šia formule.
-        cout << "Iveskite egzamino rezultatà: ";
-        studentai[i].egzas = GautiSkaiciu();
-    }
-//Vartotoją yra prašoma įvesti egzamino pažimį iš funkcijos "GautiSkaičiu()".
-    cout << "Vardas\t" << "Pavarde\t" << "\t" << "Galutinis (Vid.)" << endl;
-    cout << "----------------------------------------" << endl;
 
-    for ( int i = 0; i < s; i++ ) {
-        cout << studentai[i].vardas << "\t" << studentai[i].pavarde;
-        cout.width(29);
-        cout<< fixed << setprecision( 2 ) << ( 0.4 * studentai[i].vid + 0.6 * studentai[i].egzas ) << endl;
+        if ( !( n % 2 ) ) {
+            studentai[i].med /= 2;
+            //Apskaičiuojame mediana
+        }
+
+        studentai[i].vid = suma / n;
+//vidurki apskaiciuojame šia formule.
+        cout << "Iveskite egzamino rezultata: ";
+        studentai[i].egz = GautiSkaiciu();
     }
-//viskas išspauzdinama lentelėje.
+//Vartotoja yra prašoma ivesti egzamino pažimi iš funkcijos "GautiSkaiciu()".
+    cout << "Isvesti mediana (T) ar vidurki (N): ";
+    char ats;
+    cin >> ats;
+
+    bool mediana = false;
+
+    if ( ats == 'T' || ats == 't' ) {
+        mediana = true;
+        cout << "Vardas\t" << "Pavarde\t" << "Galutinis (Med.)" << endl;
+    } else {
+        cout << "Vardas\t" << "Pavarde\t" << "Galutinis (Vid.)" << endl;
+    }
+
+    cout << "--------------------------------" << endl;
+
+    double gal = 0;
+
+    for ( int i = 0; i < s; i++, gal = 0 ) {
+        cout << studentai[i].vardas << "\t" << studentai[i].pavarde << "\t";
+
+        if ( mediana ) {
+            gal = 0.4 * studentai[i].med;
+        } else {
+            gal = 0.4 * studentai[i].vid;
+        }
+
+        gal += 0.6 * studentai[i].egz;
+
+        cout << fixed << setprecision( 2 ) << gal << endl;
+    }
+
     return 0;
 }
+//viskas išspauzdinama lenteleje.
