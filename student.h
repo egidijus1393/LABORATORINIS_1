b40203b52c66af899eaf0a807bb98e6ee8d4ee0a
egidijus1393 <47447462+egidijus1393@users.noreply.github.com>
2019-04-10 20:27:54 +0300

    Aprašoma struktūra.

---
+++ student.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <string>
+using namespace std;
+struct Studentas {
+    string vardas;
+    string pavarde;
+    unsigned egzamino_rez  = 0;
+    double vidurkis      = 0;
+    double mediana       = 0;
+};
+///aprasoma struktura.
