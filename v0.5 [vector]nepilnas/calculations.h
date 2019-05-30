fdf97fc58d4ecd28d38fb3aa72b889b6a767a24b
egidijus1393 <47447462+egidijus1393@users.noreply.github.com>
2019-05-30 11:52:29 +0300

    Add files via upload

---
+++ v0.5 [vector]nepilnas/calculations.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include "student.h"
+#include <vector>
+using namespace std;
+void skaiciuoti_med_vid( Studentas &, const vector<unsigned> & );
+///aprasoma funkcija medianos ir vidurkio skaiciavimui.
